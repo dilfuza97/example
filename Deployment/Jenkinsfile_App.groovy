d3062d6af4bde35b7eedc4c23ec0bfb0866da7d6
Dilfuza Ibaidullaeva <47833854+dilfuza97@users.noreply.github.com>
2020-07-20 17:55:45 -0500

    Update Jenkinsfile_App.groovy

--- Deployment/Jenkinsfile_App.groovy
+++ Deployment/Jenkinsfile_App.groovy
@@ -7,8 +7,8 @@ node('master') {
   }
 
     stage("Download Terraform"){
-       sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
-        sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+       sh "wget https://releases.hashicorp.com/terraform/0.12.28/terraform_0.12.28_linux_amd64.zip"
+        sh "unzip -o terraform_0.12.28_linux_amd64.zip"
         sh "sudo mv terraform /bin"
         sh "terraform version"
 
