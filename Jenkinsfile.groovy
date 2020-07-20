28b931694ba38b1bd660958bf94c889c58d19d3d
Dilfuza Ibaidullaeva <47833854+dilfuza97@users.noreply.github.com>
2020-07-20 15:19:16 -0500

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,47 @@
+node('master') {
+  withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: '', credentialsId: 'aws', secretKeyVariable: '']]) {
+    // some block
+
+  stage('Clone repo') {
+    git credentialsId: 'github', url: 'https://github.com/dilfuza97/Docker_Jenkins_py.git'
+  }
+
+   stage('Check awscli') {
+      try {
+        // Trying to run terraform command
+        env.terraform  = sh returnStdout: true, script: 'aws --version'
+        echo """
+        echo AWS CLI is already installed version ${env.terraform}
+        """
+        } catch(er) {
+              // if terraform does not installed in system stage will install the terraform
+               stage('Installing AWS CLI') {
+                 sh """
+                 yum install awscli -y
+                 """
+               }
+            }
+          }
+   stage('Docker build') {
+           dir("${WORKSPACE}") {
+             sh "docker build -t http-server ."
+           }
+      }
+
+     
+    stage('Docker push') {
+
+           dir("${WORKSPACE}") {
+             sh "docker tag http-server:latest 608022717509.dkr.ecr.us-east-1.amazonaws.com/http-server:latest"
+      }
+           dir("${WORKSPACE}") {
+             sh "docker push 608022717509.dkr.ecr.us-east-1.amazonaws.com/http-server:latest"
+     }
+  }
+    stage('Docker run') {
+           dir("${WORKSPACE}") {
+             sh "docker run -dti -p 85:8080 385571246247.dkr.ecr.us-east-1.amazonaws.com/task"
+           }
+         }
+      }
+    }
